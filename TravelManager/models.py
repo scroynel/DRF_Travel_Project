6c253d691c2a53766d049912d1918e8f0c8edb93
Vladyslav <scroynel@gmail.com>
2026-07-01 22:50:36 +0200

    Creating models for a project

--- TravelManager/models.py
+++ TravelManager/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    description = models.TextField(blank=True, null=True)
+    start_date = models.DateField(blank=True, null=True)
+
+
+class Place(models.Model):
+    external_id = models.IntegerField(unique=True)
+    title = models.CharField(max_length=100)
+
+
+class ProjectPlace(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    notes = models.TextField(blank=True, default='')
+    visited = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        unique_together = ('project', 'place')
