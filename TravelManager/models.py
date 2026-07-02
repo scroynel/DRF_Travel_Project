e590301c56dc5adb33f9ecae737c2aad2684b574
Vladyslav <scroynel@gmail.com>
2026-07-02 21:31:31 +0200

    Cannot delete Project if it has visited places

--- TravelManager/models.py
+++ TravelManager/models.py
@@ -7,13 +7,21 @@ class Project(models.Model):
     start_date = models.DateField(blank=True, null=True)
 
 
+    def __str__(self):
+        return self.name
+
+
 class Place(models.Model):
     external_id = models.IntegerField(unique=True)
     title = models.CharField(max_length=100)
 
 
+    def __str__(self):
+        return self.title
+
+
 class ProjectPlace(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='place')
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
     notes = models.TextField(blank=True, default='')
     visited = models.BooleanField(default=False)
--- TravelManager/views.py
+++ TravelManager/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, exceptions
 from .models import Project, Place, ProjectPlace
 from .serializers import ProjectSerializer, PlaceSerializer, ProjectPlaceSerializer
 
@@ -6,6 +6,13 @@ from .serializers import ProjectSerializer, PlaceSerializer, ProjectPlaceSeriali
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+
+    def destroy(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.place.filter(visited=True).exists():
+            raise exceptions.ValidationError(f'Cannot delete Project - {obj.name} with visited places')
+        return super().destroy(request, *args, **kwargs)
 
 
 class PlaceViewSet(viewsets.ModelViewSet):
